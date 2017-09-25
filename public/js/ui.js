9d8902dfad8c261b7dbc636c93fa5f15714cf0ab
Peter Širka <petersirka@gmail.com>
2017-09-25 15:10:38 +0200

    Add shortcuts.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1234,3 +1234,110 @@ COMPONENT('autocomplete', function(self) {
 		self.visible(true);
 	};
 });
+
+COMPONENT('shortcuts', function(self) {
+
+	var items = [];
+	var length = 0;
+
+	self.singleton();
+	self.readonly();
+	self.blind();
+
+	self.make = function() {
+		$(window).on('keydown', function(e) {
+			if (length) {
+
+				for (var i = 0; i < length; i++) {
+					var o = items[i];
+					if (o.fn(e)) {
+						if (o.prevent) {
+							e.preventDefault();
+							e.stopPropagation();
+						}
+						setTimeout(function(o, e) {
+							o.callback(e);
+						}, 100, o, e);
+					}
+				}
+			}
+		});
+	};
+
+	self.register = function(shortcut, callback, prevent) {
+		var builder = [];
+		shortcut.split('+').trim().forEach(function(item) {
+			var lower = item.toLowerCase();
+			switch (lower) {
+				case 'ctrl':
+				case 'alt':
+				case 'shift':
+					builder.push('e.{0}Key'.format(lower));
+					return;
+				case 'win':
+				case 'meta':
+				case 'cmd':
+					builder.push('e.metaKey');
+					return;
+				case 'space':
+					builder.push('e.keyCode===32');
+					return;
+				case 'tab':
+					builder.push('e.keyCode===9');
+					return;
+				case 'esc':
+					builder.push('e.keyCode===27');
+					return;
+				case 'enter':
+					builder.push('e.keyCode===13');
+					return;
+				case 'backspace':
+				case 'del':
+				case 'delete':
+					builder.push('(e.keyCode===8||e.keyCode===127)');
+					return;
+				case 'up':
+					builder.push('e.keyCode===38');
+					return;
+				case 'down':
+					builder.push('e.keyCode===40');
+					return;
+				case 'right':
+					builder.push('e.keyCode===39');
+					return;
+				case 'left':
+					builder.push('e.keyCode===37');
+					return;
+				case 'f1':
+				case 'f2':
+				case 'f3':
+				case 'f4':
+				case 'f5':
+				case 'f6':
+				case 'f7':
+				case 'f8':
+				case 'f9':
+				case 'f10':
+				case 'f11':
+				case 'f12':
+					var a = item.toUpperCase();
+					builder.push('e.key===\'{0}\''.format(a));
+					return;
+				case 'capslock':
+					builder.push('e.which===20');
+					return;
+			}
+
+			var num = item.parseInt();
+			if (num)
+				builder.push('e.which===' + num);
+			else
+				builder.push('e.key===\'{0}\''.format(item));
+
+		});
+
+		items.push({ fn: new Function('e', 'return ' + builder.join('&&')), callback: callback, prevent: true });
+		length = items.length;
+		return self;
+	};
+});
