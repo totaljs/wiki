870b77fc8af95ea87efed59f4a6b0c24bd08025d
Peter Širka <petersirka@gmail.com>
2017-10-03 22:04:25 +0200

    Code improvement.

--- definitions/markdown.js
+++ definitions/markdown.js
@@ -1,9 +1,13 @@
+const REG_LINKS = /"\@\d+[0-9a-z]+(#|")/g;
+const REG_HREF = /href="\//g;
+const REG_TABLE = /<table/g;
+
 global.marked = require('marked');
 global.marked.setOptions({ gfm: true, breaks: true, sanitize: true, tables: true });
 
 global.markdown = function(text) {
-	return marked(text).replace(/<table/g, '<table class="table table-bordered"').replace(/"\@\d+[0-9a-z]+(#|")/g, function(id) {
+	return marked(text).replace(REG_TABLE, '<table class="table table-bordered"').replace(REG_LINKS, function(id) {
 		var link = F.global.pages.findItem('id', id.substring(2, id.length - 1));
 		return (link ? ('"/' + link.url + '/' + id.substring(id.length - 1)) : id);
-	}).replace(/href="\//g, 'class="jrouting" href="/');
+	}).replace(REG_HREF, 'class="jrouting" href="/');
 };
